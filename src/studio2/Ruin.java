e5d4664f7b118639a7350a77a80b1f4d53a4f4a1
tyholloway <hty@wustl.edu>
2026-01-28 13:22:21 -0600

    Commit1

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Ruin {
+
+
+public static void main(String[] args) {
+
+    double winOdds;
+    Scanner scan = new Scanner(System.in);
+
+
+
+}
+
+
+
+}
